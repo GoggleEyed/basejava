8c968776cf40af14107378b219c60ccba10b7960
GoggleEyed <GoggleEyed@protonmail.com>
2024-03-11 15:07:49 +0300

    Implement ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        if (size >= storage.length) {
+            throw new ArrayIndexOutOfBoundsException("Array storage is full");
+        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (i < size && !storage[i].uuid.equals(uuid)) {
+            i++;
+        }
+        if (i == size) {
+            return;
+        }
+        for (; i < size - 1; i++) {
+            storage[i] = storage[i + 1];
+        }
+        storage[i] = null;
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
