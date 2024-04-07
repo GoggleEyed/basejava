043c252695e43bd0c23e27cf3da0fab7960b56a6
GoggleEyed <GoggleEyed@protonmail.com>
2024-04-07 22:25:40 +0300

    Update ResumeServlet

--- src/com/goggleeyed/webapp/Config.java
+++ src/com/goggleeyed/webapp/Config.java
@@ -10,8 +10,7 @@ import java.nio.file.Files;
 import java.util.Properties;
 
 public class Config {
-    //    private static final File PROPS = new File("./config/resumes.properties");
-    private static final File PROPS = new File("/home/goggleeyed/JavaCourse/basejava/config/resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -38,5 +37,15 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
+    }
+
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
--- src/com/goggleeyed/webapp/web/ResumeServlet.java
+++ src/com/goggleeyed/webapp/web/ResumeServlet.java
@@ -1,17 +1,28 @@
 package com.goggleeyed.webapp.web;
 
 import com.goggleeyed.webapp.Config;
+import com.goggleeyed.webapp.model.ContactType;
 import com.goggleeyed.webapp.model.Resume;
 import com.goggleeyed.webapp.storage.Storage;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+
+    private Storage storage; // = Config.get().getStorage();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
@@ -21,17 +32,31 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body><table border=1 >");
-        out.println("<tr><th>uuid</th><th>fullName</th><tr>");
-        Storage storage = Config.get().getStorage();
-        List<Resume> resumes = storage.getAllSorted();
-        for (Resume resume : resumes) {
-            out.println("<tr><td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td><tr>");
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>\n" +
+                        "<head>\n" +
+                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                        "    <title>Список всех резюме</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<section>\n" +
+                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                        "    <tr>\n" +
+                        "        <th>Имя</th>\n" +
+                        "        <th>Email</th>\n" +
+                        "    </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
+                            "</tr>\n");
         }
-        out.println("</table></body></html>");
-
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        writer.write("</table>\n" +
+                "</section>\n" +
+                "</body>\n" +
+                "</html>\n");
     }
 }
